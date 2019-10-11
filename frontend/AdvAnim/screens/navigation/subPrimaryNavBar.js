5a4b3ef2f2f460b2dfd5b3cf431ec40a2f0be454
Alexander Beaver <alex@beaver.dev>
2019-10-11 09:19:43 -0700

    Finished authentication scheme. Will now add "get post" mapping

--- frontend/AdvAnim/screens/navigation/subPrimaryNavBar.js
+++ frontend/AdvAnim/screens/navigation/subPrimaryNavBar.js
@@ -1,8 +1,12 @@
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import {Inbox} from '../primary/inbox';
 import {NewPost} from '../primary/newPost';
+import {Settings} from '../primary/settings';
 const SubPrimary = createBottomTabNavigator({
     Inbox: {screen: Inbox},
-    NewPost: {screen: NewPost}
+    NewPost: {screen: NewPost},
+    Settings: {screen: Settings}
+},{
+    title: "EggChat"
 });
 export default SubPrimary;
